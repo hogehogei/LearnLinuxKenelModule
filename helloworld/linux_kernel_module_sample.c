9b5be9c1b46b7d79438e2e5c70bd0cba2da55c0b
hogehogei <matsuryo00@gmail.com>
2023-05-09 14:01:11 +0900

    Initial commit.

---
+++ helloworld/linux_kernel_module_sample.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init helloworld_init(void)
+{
+    pr_info( "Hello world initialization!\n" );
+    return 0;
+}
+
+static void __exit helloworld_exit(void)
+{
+    pr_info( "Hello world exit\n" );
+}
+
+module_init(helloworld_init);
+module_exit(helloworld_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR( "HogeHogei <matsuryo00@gmail.com>" );
+MODULE_DESCRIPTION( "Linux kernel module skeleton" );
